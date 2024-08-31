e9c745270686e4b263fda1929acf8b2c29c98e29
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-08-31 23:54:38 +0000

    UPDATE: Firebase login works!

--- ctf-playground/src/components/Navbar.js
+++ ctf-playground/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
     <nav className="bg-blue-700 p-4 text-white">
         <div className="container mx-auto">
-            <Link to="/playground" className='text-2xl font-bold'> &larr; </Link>
+            <Link to="/playground" className='text-2xl font-bold'> 🏠 </Link>
             <Link to="/" className="text-2xl font-bold">CTF Playground</Link>
         </div>
     </nav>
